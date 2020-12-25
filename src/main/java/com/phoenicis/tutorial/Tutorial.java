d1c13689b3a5398eb9d9080ca27ba8fc4a37e934
phoenicisestuans <hallmichaeldavid339@gmail.com>
2020-12-25 13:38:27 -0800

    Initial commit

---
+++ src/main/java/com/phoenicis/tutorial/Tutorial.java
@@ -0,0 +1,14 @@
+package com.phoenicis.tutorial;
+
+import com.phoenicis.tutorial.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+public class Tutorial implements ModInitializer {
+
+    public static final String MOD_ID = "pht";
+
+    @Override
+    public void onInitialize() {
+        ModItems.registerItems();
+    }
+}
---
+++ src/main/java/com/phoenicis/tutorial/registry/ModItems.java
@@ -0,0 +1,15 @@
+package com.phoenicis.tutorial.registry;
+
+import com.phoenicis.tutorial.Tutorial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item RUBY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
+    public static void registerItems() {
+        Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID), "ruby", RUBY);
+    }
+}
